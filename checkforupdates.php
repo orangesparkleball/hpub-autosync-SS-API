427a7a228d8033fddd4fc1cc0d18350310e8eb9e
Will Youmans <will@klanoma.com>
2011-09-27 20:34:18 -0600

    test

--- checkforupdates.php
+++ checkforupdates.php
@@ -5,6 +5,6 @@ require_once('config.php');
 if(isset($_GET['key']) && $_GET['key'] == $api_key){
   die(json_encode(array('isError' => '0', 'code' => '1', 'message' => 'An update is available', 'size' => '5000'))); 
 } else {
-  die(json_encode(array('isError' => '0', 'code' => '-1', 'message' => 'invalid API key', 'size' => '0')));
+  die(json_encode(array('isError' => '1', 'code' => '-1', 'message' => 'invalid API key', 'size' => '0')));
 }
 ?>
