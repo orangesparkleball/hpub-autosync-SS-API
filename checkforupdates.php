c424c0c1fddee2ecbbfee6c02c14c111a1f73384
Will Youmans <will@klanoma.com>
2011-09-27 20:35:15 -0600

    get real file size

--- checkforupdates.php
+++ checkforupdates.php
@@ -3,7 +3,7 @@
 require_once('config.php');
 
 if(isset($_GET['key']) && $_GET['key'] == $api_key){
-  die(json_encode(array('isError' => '0', 'code' => '1', 'message' => 'An update is available', 'size' => '5000'))); 
+  die(json_encode(array('isError' => '0', 'code' => '1', 'message' => 'An update is available', 'size' => filesize ( 'temp.hpub' )))); 
 } else {
   die(json_encode(array('isError' => '1', 'code' => '-1', 'message' => 'invalid API key', 'size' => '0')));
 }
