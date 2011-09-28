11e8eb3891fecb0ad82007ee92e4a47cbb8bd348
Will Youmans <will@klanoma.com>
2011-09-27 20:33:25 -0600

    use gmt for api_key

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 $shared_secret = 'This is a super top secret string!';
-$time_factor = date('m/d/y h');
+$time_factor = gmdate('m/d/y h');
 
 $api_key = md5(md5($shared_secret).md5($time_factor));
 
